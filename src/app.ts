4367aaa2c5a50ca505a911a0237d8ac6cbd99888
MANTU KUMAR <mantukumarverma2004@gmail.com>
2024-09-21 00:08:02 +0530

    update in xml

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ export const epsonRouter = express.Router();
 epsonRouter.post("/print", async (req: Request, res: Response) => {
   // return header Content-Type: text/xml; charset=utf-8
   const url = `http://epson-cloud-printer.onrender.com/document.xml`; // URL of your ePOS XML document
-
   try {
     const response = await axios.post(
       `https://pos-cloud-link.epson.com/public/api/v1/devices/${process.env.EPSON_DEVICE_ID}/cloud/print`,
